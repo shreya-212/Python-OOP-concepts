df990b307a6f685eb5c40082b4c85376837205db
shreya-212 <shreyaa.srs@gmail.com>
2025-10-07 20:50:21 +0530

    added class_and_object example

---
+++ classes_and_object.py
@@ -0,0 +1,14 @@
+class Employee:
+    #class attributes         
+    language="Python"    
+    salary=50000
+
+#create objects
+emp1=Employee()           
+print(emp1.language,emp1.salary)
+
+emp2=Employee()
+#instance attribute
+emp2.language="javascript"   
+print(emp2.language,emp2.salary)
+
